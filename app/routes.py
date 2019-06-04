2f000eb55d6f5a3667fdff44808c6f43cf3f16c2
Sourabh Belekar <sourabh.belekar@gmail.com>
2019-06-04 22:42:21 +0000

    added device registration route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from app import app
+from app import app,db
+from app.models import Device
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -8,4 +10,13 @@ def index():
 
 @app.route('/register', methods=['POST'])
 def register():
-	return "Success"
+	data=request.get_json()
+	temp_device=db.session.query(Device).filter(Device.devicename==data['name'])
+	if(temp_device.count()>0):
+		temp_device.first().ipaddress=data['ipaddr']
+		db.session.commit()
+		return "Updated device details"
+	d=Device(devicename=data['name'],ipaddress=data['ipaddr'])
+	db.session.add(d)
+	db.session.commit()
+	return "Registered new Device"
