0db9221f26a0335732756d2a10c2e22f7996335f
Sourabh Belekar <sourabh.belekar@gmail.com>
2019-06-09 17:42:00 +0100

    Added updateTime field to Device model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from app import db
+from datetime import datetime
 
 class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    devicename = db.Column(db.String(64), index=True, unique=True)
-    ipaddress = db.Column(db.String(120), index=True, unique=True)
+    devicename = db.Column(db.String(64), index=True)
+    ipaddress = db.Column(db.String(120), index=True)
+    updatetime = db.Column(db.DateTime,default=datetime.utcnow)
 
     def __repr__(self):
         return '<Device {} {}>'.format(self.devicename,self.ipaddress)    
