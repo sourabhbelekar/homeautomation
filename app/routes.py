4e7ff907014ec7eb2e8e428a6beae5e8ba8edc4d
Sourabh Belekar <sourabh.belekar@gmail.com>
2019-06-09 17:41:11 +0100

    updated device registration method

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,33 @@
 from app import app,db
 from app.models import Device
-from flask import request
+from flask import request,render_template
+import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
+    ct=datetime.datetime.utcnow()
+    lt = ct - datetime.timedelta(seconds=10)
+    q=db.session.query(Device).filter(Device.updatetime > lt).all()
+    
+    urls=q
+    #for device in q:
+    #    urls.append('http://'+device.ipaddress+'/')
+    iframe = "http://www.google.com"
+
+    return render_template('index.html', iframe=iframe,devices=urls)
+
+
+@app.route('/test')
+def test():
 	return "haha"
+
+@app.route('/control',methods = ['POST'])
+def control():
+        if request.method == 'POST':
+                result = request.form
+                return render_template("control.html",iframe=request.form["devices"])
+
 
 
 @app.route('/register', methods=['POST'])
@@ -14,9 +36,10 @@ def register():
 	temp_device=db.session.query(Device).filter(Device.devicename==data['name'])
 	if(temp_device.count()>0):
 		temp_device.first().ipaddress=data['ipaddr']
-		db.session.commit()
-		return "Updated device details"
+		temp_device.first().updatetime=datetime.datetime.utcnow()
+                db.session.commit()
+		return "Updated Device"
 	d=Device(devicename=data['name'],ipaddress=data['ipaddr'])
 	db.session.add(d)
 	db.session.commit()
-	return "Registered new Device"
+	return "Added New Device"
